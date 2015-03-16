e1215ec6539a4ab227828169f589aea78b71d935
Guillermo Gutiérrez Doral <guillermo.gutierrez@twincoders.com>
2015-03-16 18:07:15 +0100

    Integrated missing applicationDidEnterBackground method call in demo project

--- TwinPushSDKDemo/TwinPushSDKDemo/AppDelegate.m
+++ TwinPushSDKDemo/TwinPushSDKDemo/AppDelegate.m
@@ -63,16 +63,17 @@
     [[TwinPushManager manager] applicationWillResignActive:application];
 }
 
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [[TwinPushManager manager] applicationDidEnterBackground:application];
+    [[TwinPushManager manager] setApplicationBadgeCount:0];
+}
+
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 
     // Reset App icon badge
-    [[TwinPushManager manager] setApplicationBadgeCount:0];
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     [[TwinPushManager manager] setApplicationBadgeCount:0];
 }
 
