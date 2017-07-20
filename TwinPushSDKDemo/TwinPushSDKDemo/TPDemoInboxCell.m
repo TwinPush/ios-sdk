a4fad12576fd3d7f5a9eb8cf5f8a192f1748aa0d
Guillermo Gutiérrez <guillermo.gutierrez@twincoders.com>
2017-07-20 11:40:40 +0200

    SDK Demo will now show notification cells in bold font when the notification is not opened yet

--- TwinPushSDKDemo/TwinPushSDKDemo/TPDemoInboxCell.m
+++ TwinPushSDKDemo/TwinPushSDKDemo/TPDemoInboxCell.m
@@ -38,4 +38,31 @@
     return YES;
 }
 
+
+- (UIFont*)boldFont {
+    static UIFont* font = nil;
+    if (font == nil) {
+        font = [UIFont fontWithName:@"MuseoSans-700" size:13];
+    }
+    return font;
+}
+
+- (UIFont*)normalFont {
+    static UIFont* font = nil;
+    if (font == nil) {
+        font = [UIFont fontWithName:@"MuseoSans-300" size:13];
+    }
+    return font;
+}
+
+- (void)setNotification:(TPNotification *)notification {
+    [super setNotification:notification];
+    
+    if ([notification isKindOfClass:[TPInboxNotification class]]) {
+        TPInboxNotification* inboxNotification = (TPInboxNotification*)notification;
+        self.notificationDescriptionLabel.font = inboxNotification.isOpened ? [self normalFont] : [self boldFont];
+        self.notificationDateLabel.font = inboxNotification.isOpened ? [self normalFont] : [self boldFont];
+    }
+}
+
 @end
